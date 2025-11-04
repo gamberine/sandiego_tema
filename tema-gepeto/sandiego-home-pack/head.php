fe481172d763d3f84db7b9fd66f86378180655b6
gamberine <gamberine@gmail.com>
2025-11-04 05:31:20 -0300

    dfgfyryg

---
+++ tema-gepeto/sandiego-home-pack/head.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Tema_Dev_Gamb
+ * @since Tema Dev-Gamb 1.0
+ */
+?>
+<!DOCTYPE html>
+<!-- <html < ?php language_attributes(); ?> id="top" class="user-select-none"> -->
+<html <?php language_attributes(); ?> id="top">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#0d5963">
+  <meta name="theme-color" media="(prefers-color-scheme: light)" content="#0d5963" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="#0d5963">
+  <meta name="msapplication-navbutton-color" content="#0d5963">
+
+  <!-- <link rel="icon" href="< ?php echo esc_url(get_field('favicon_customizado', 'option')); ?>" type="image/png"> -->
+
+  <?php wp_head(); ?>
+
+</head>
