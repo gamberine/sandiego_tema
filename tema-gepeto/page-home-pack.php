1c39473f928eb9c4f8e1d48c688f31b24d8b8cb1
gamberine <gamberine@gmail.com>
2025-11-07 05:53:10 -0300

    fghgfdsa

--- tema-gepeto/page-home-pack.php
+++ tema-gepeto/page-home-pack.php
@@ -4,7 +4,8 @@ get_header();
 ?>
 <main id="primary" class="site-main sandiego">
   <?php if (have_rows('home_sections')): while (have_rows('home_sections')): the_row();
-      if (get_row_layout() == 'hero') get_template_part('template-parts/sections/section', 'banner');
+      if (get_row_layout() == 'hero') get_template_part('template-parts/sections/section', 'hero');
+            if (get_row_layout() == 'hero') get_template_part('template-parts/sections/section', 'banner');
       if (get_row_layout() == 'search') get_template_part('template-parts/sections/section', 'search');
       if (get_row_layout() == 'services') get_template_part('template-parts/sections/section', 'services');
       if (get_row_layout() == 'numbers') get_template_part('template-parts/sections/section', 'numbers');
